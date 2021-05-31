e9f49442f2d7f4f76750dc7f7a9bea9b310005ae
hilleriteauf <46034382+hilleriteauf@users.noreply.github.com>
2021-05-31 14:26:49 +0200

    Updates

---
+++ precache-manifest.accb39668d80a0f41085a7d7a63c014a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230ad3c2a15c1d26eeea8cba1143b917",
+    "url": "/test_portfolio/index.html"
+  },
+  {
+    "revision": "108247c3289ae89f520e",
+    "url": "/test_portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d00294c37e4ca05f4ed2",
+    "url": "/test_portfolio/static/css/main.fcf2461e.chunk.css"
+  },
+  {
+    "revision": "108247c3289ae89f520e",
+    "url": "/test_portfolio/static/js/2.8e331091.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/test_portfolio/static/js/2.8e331091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00294c37e4ca05f4ed2",
+    "url": "/test_portfolio/static/js/main.5f8c75d4.chunk.js"
+  },
+  {
+    "revision": "126851413d59fd685048",
+    "url": "/test_portfolio/static/js/runtime-main.9985bf23.js"
+  }
+]);
